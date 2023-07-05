6fedb3962c61bdb8d677edfee1c9afd26a0ba632
Ashutosh Sunil Gore <130917119+Ashugore48@users.noreply.github.com>
2023-07-05 11:19:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-
+	let Str = "";
+	for(let i = 0; i < str.length;i++){
+		if(str.charAt(i) === ' '){
+			continue;
+		}
+		Str = Str + str.charAt(i);
+	}
+	let revStr = Str.split('').reverse().join('')
+	console.log(revStr);
+	if(Str === revStr){
+	    return true;
+	}else{
+	    return false;
+	}
 }
 module.exports = palindrome
